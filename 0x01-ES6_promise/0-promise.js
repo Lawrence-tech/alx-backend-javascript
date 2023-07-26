269a096e124f52758a142664666a15c5d947093d
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-27 01:34:37 +0300

    0. Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,13 @@
-export default function getResponseFromAPI() {
-  return new Promise(() => {});
+// 0-promise.js
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Here, you can perform your API call or any asynchronous operation.
+    // For demonstration purposes, let's resolve with a sample response after a short delay.
+    setTimeout(() => {
+      const sampleResponse = { data: 'Sample response from API' };
+      resolve(sampleResponse);
+    }, 1000); // Simulating an asynchronous delay of 1 second.
+  });
 }
+
+export default getResponseFromAPI;
