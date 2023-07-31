282998595f6b8d0bc83a2138e91272be4c279b64
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-31 10:44:47 +0300

    eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,10 @@
-export default function handleResponseFromAPI(promise) {
+function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-    .finally(() => {
+    .then((data) => {
       console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch((error) => {
+      console.log('Got a response from the API');
+      return new Error();
     });
-}
