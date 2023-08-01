fca147a2fc533a67c617887ae69405d09ce8cce1
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-01 14:57:21 +0300

    2. A Course, Getters, and Setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Setter for name
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a strin');
+    }
+    this._name = newName;
+  }
+
+  // Getter for length
+  get length() {
+    return this._length;
+  }
+
+  // Setter for length
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  // Getter for students
+  get students() {
+    return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw new TypeError('Students must be an array');
+    }
+    this._students = newStudents;
+  }
+}
+
+export default HolbertonCourse;
