bc0ea6cb7f7bd73167c3cc04e7f0f71b377d65dc
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-22 10:20:32 +0300

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,23 @@
-// Handle multiple promises
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
+export async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  let result = [];
+
+  try {
+    const [userData, photoData] = await Promise.all([userPromise, photoPromise]);
+    result.push({ status: userData.status, value: userData.value });
+    result.push({ status: photoData.status, value: photoData.value });
+  } catch (error) {
+    result.push({ status: error.status, value: error.value });
+    result.push({ status: 'rejected', value: `Error: ${error.message}` });
+  }
+
+  return result;
 }
 
-export default handleProfileSignup;
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+
