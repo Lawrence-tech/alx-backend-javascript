08de50b274ac644806a2ae1f3a7c79ab0949f776
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-22 10:05:42 +0300

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,18 @@
+/* Handle multiple successful promises
+    Put () because return Promise resolve object
+*/
 import { createUser, uploadPhoto } from './utils';
 
-// Handles the signup process for user profile by combining photo upload and user creation.
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      const { body: photoResult } = values[0];
+      const { body } = values[0];
       const { firstName, lastName } = values[1];
 
-      console.log(`Photo upload: ${photoResult}, User: ${firstName} ${lastName}`);
+      console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
-      console.log('System temporarily unavailable for signup.');
+      console.log('Signup system offline');
     });
 }
 
