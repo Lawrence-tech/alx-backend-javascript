7fc75a02186b9855f882f05fb17a3f794ab63fd5
Lawrence-tech <lawrencesagini2@gmail.com>
2023-09-20 05:17:29 +0300

    Added requirement to read asynchronously. Also added full server directory

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,24 +2,24 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    // Read the database file synchronously
     const data = fs.readFileSync(path, 'utf8');
+    const result = [];
+    data.split('\n').forEach((data) => {
+      result.push(data.split(','));
+    });
+    result.shift();
+    const newis = [];
+    result.forEach((data) => newis.push([data[0], data[3]]));
+    const fields = new Set();
+    newis.forEach((item) => fields.add(item[1]));
+    const final = {};
+    fields.forEach((data) => { (final[data] = 0); });
+    newis.forEach((data) => { (final[data[1]] += 1); });
+    console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+    Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+  } catch (E) {
+    throw Error('Cannot load the database');
+  }
+}
 
-    // Split the file content by lines
-    const lines = data.split('\n').filter(Boolean); // Remove empty lines
-
-    // Initialize counters for each field
-    const fields = {};
-    
-    // Iterate through each line
-    for (const line of lines) {
-      const student = line.split(',');
-      const field = student[3].trim(); // Assuming field is in the 4th column (index 3)
-      
-      if (field) {
-        if (fields[field]) {
-          fields[field].push(student[0].trim()); // Assuming student's name is in the 1st column (index 0)
-        } else {
-          fields[field] = [student[0].trim()];
-        }
-     
+module.exports = countStudents;
